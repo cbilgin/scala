e911639289a85be7b985b76b12bb46534fbad7f3
cbilgin-cr <chataibilgin@gmail.com>
2016-10-10 17:32:53 -0700

    Add a test for the filter bug.

--- funsets/src/test/scala/funsets/FunSetSuite.scala
+++ funsets/src/test/scala/funsets/FunSetSuite.scala
@@ -27,14 +27,6 @@ class FunSetSuite extends FunSuite {
    */
 
   /**
-   * Tests are written using the "test" operator and the "assert" method.
-   */
-  // test("string take") {
-  //   val message = "hello, world"
-  //   assert(message.take(5) == "hello")
-  // }
-
-  /**
    * For ScalaTest tests, there exists a special equality operator "===" that
    * can be used inside "assert". If the assertion fails, the two values will
    * be printed in the error message. Otherwise, when using "==", the test
@@ -43,10 +35,6 @@ class FunSetSuite extends FunSuite {
    * Try it out! Change the values so that the assertion fails, and look at the
    * error message.
    */
-  // test("adding ints") {
-  //   assert(1 + 2 === 3)
-  // }
-
 
   import FunSets._
 
@@ -77,26 +65,26 @@ class FunSetSuite extends FunSuite {
     val s1 = singletonSet(1)
     val s2 = singletonSet(2)
     val s3 = singletonSet(3)
+    val s4 = singletonSet(4)
+    val s5 = singletonSet(5)
+    val s7 = singletonSet(7)
+    val s1000 = singletonSet(1000)
   }
 
   /**
-   * This test is currently disabled (by using "ignore") because the method
-   * "singletonSet" is not yet implemented and the test would fail.
-   *
-   * Once you finish your implementation of "singletonSet", exchange the
-   * function "ignore" by "test".
-   */
+    * This test is currently disabled (by using "ignore") because the method
+    * "singletonSet" is not yet implemented and the test would fail.
+    *
+    * Once you finish your implementation of "singletonSet", exchange the
+    * function "ignore" by "test".
+    *
+    * We create a new instance of the "TestSets" trait, this gives us access
+    * to the values "s1" to "s3".
+    * The string argument of "assert" is a message that is printed in case
+    * the test fails. This helps identifying which assertion failed.
+    */
   test("singletonSet(1) contains 1") {
-
-    /**
-     * We create a new instance of the "TestSets" trait, this gives us access
-     * to the values "s1" to "s3".
-     */
     new TestSets {
-      /**
-       * The string argument of "assert" is a message that is printed in case
-       * the test fails. This helps identifying which assertion failed.
-       */
       assert(contains(s1, 1), "Singleton")
     }
   }
@@ -110,5 +98,10 @@ class FunSetSuite extends FunSuite {
     }
   }
 
-
+  test("Filter less than 5"){
+    new TestSets {
+      val s = union(union(union(union(union(s1, s3), s4), s5), s7), s1000)
+      printSet(filter(s, _ < 5))
+    }
+  }
 }
